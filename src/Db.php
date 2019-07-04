514687821f8197036f96374aea96c0952e4b8976
Ваше Имя <you@example.com>
2019-07-04 07:11:26 +0200

    Fix Db Class (from comments to commit)

--- src/Db.php
+++ src/Db.php
@@ -7,52 +7,48 @@ use \MongoDB\Database;
 
 class Db
 {
-    static $dm;
+    private $dm;
 
     public function __construct()
     {
-        if(!(static::$dm instanceof Datatbase)) {
-            $env = getenv();
+        $env = getenv();
 
-            define('MONGO_HOST', $env['MONGO_SERVER']);
-            define('MONGO_DB_USERNAME', $env['MONGO_INITDB_ROOT_USERNAME']);
-            define('MONGO_DB_PASSWORD', $env['MONGO_INITDB_ROOT_PASSWORD']);
-            //define('MONGO_DB_USERNAME', 'admin');
-            //define('MONGO_DB_PASSWORD', 'admin');
-            define('MONGO_DB_NAME', $env['MONGO_INITDB_DATABASE']);
+        $mongoHost = $env['MONGO_SERVER'];
+        $mongoUsername = $env['MONGO_INITDB_ROOT_USERNAME'];
+        $mongoPassword = $env['MONGO_INITDB_ROOT_PASSWORD'];
+        $mongoDatabase = $env['MONGO_INITDB_DATABASE'];
 
-            $username_password = MONGO_DB_USERNAME . ':' . MONGO_DB_PASSWORD . '@';
-            $host_and_port = MONGO_HOST . ':' . '27017';
-            $mongoClient = new Client('mongodb://' . $username_password . $host_and_port, [], [
-                'typeMap' => [
-                    'array' => 'array',
-                    'document' => 'array',
-                    'root' => 'array',
-                ],
-            ]);
+        $usernamePassword = $mongoUsername . ':' . $mongoPassword . '@';
+        $hostAndPort = $mongoHost . ':' . '27017';
+        $mongoClient = new Client('mongodb://' . $usernamePassword . $hostAndPort, [], [
+            'typeMap' => [
+                'array' => 'array',
+                'document' => 'array',
+                'root' => 'array',
+            ],
+        ]);
 
-            static::$dm = $mongoClient->selectDatabase(MONGO_DB_NAME);
-        }
+        $this->dm = $mongoClient->selectDatabase($mongoDatabase);
     }
 
     public function createCollection(string $name)
     {
-        static::$dm->createCollection($name);
+        $this->dm->createCollection($name);
     }
 
     public function createDocument(string $collection, array $document)
     {
-        static::$dm->selectCollection($collection)->insertOne($document);
+        $this->dm->selectCollection($collection)->insertOne($document);
     }
 
     public function getDocuments(string $collection) : array
     {
-        return static::$dm->selectCollection($collection)->find([])->toArray();
+        return $this->dm->selectCollection($collection)->find([])->toArray();
     }
 
     public function pushToArray(string $collection, array $document, string $arrayName, $arrayElement)
     {
-        static::$dm->selectCollection($collection)->updateOne(
+        $this->dm->selectCollection($collection)->updateOne(
             $document,
             ['$push' => [$arrayName => $arrayElement]]
         );
